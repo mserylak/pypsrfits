9435a831f170235ea3d912d0e88a831971db0a5c
Maciej Serylak <mserylak@gmail.com>
2017-02-20 16:39:26 +0200

    Debugged setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name="pypsrfits",
+      version="1.0",
       description="Simple code to read search-mode PSRFITS data into numpy array.",
       author="Paul Demorest and Maciej Serylak",
       author_email="mserylak@ska.ac.za",
-      packages=find_packages(),
+      py_modules=["pypsrfits"],
       url='https://github.com/mserylak/pypsrfits',
       license="Academic Free License 3.0",
       classifiers=[
-          "Development Status :: 1 - Beta",
+          "Development Status :: 4 - Beta",
           "Intended Audience :: Developers",
-          "License :: OSI Approved :: AFL-3.0 License",
+          "License :: OSI Approved :: Academic Free License (AFL)",
           "Operating System :: OS Independent",
           "Programming Language :: Python",
-          "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Topic :: Software Development :: Libraries :: Python Modules",
           "Topic :: Scientific/Engineering :: Astronomy"],
       platforms=["OS Independent"],
-      keywords="psrfits",
+      keywords="pulsar psrfits meerkat ska",
       zip_safe=False,
       install_requires=['numpy', 'pyfits'])
